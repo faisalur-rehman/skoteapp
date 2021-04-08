899f7d7fc34a027c48b9a156e2f3a8dacc67ed98
faisalur-rehman <66237466+faisalur-rehman@users.noreply.github.com>
2021-04-08 13:24:21 +0500

    Made get, post and patch request for usp.

--- src/pages/app/UniqueSelling.js
+++ src/pages/app/UniqueSelling.js
@@ -1,25 +1,75 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Field, ErrorMessage } from "formik"
 import FormikComponent from "./Formik"
 import { Row, Col, Button } from "reactstrap"
+import { formGetData, formPostData, patchData } from "./ApiRequest"
 
-const initialValues = { sellingPoint: "", strength: "", whyYou: "" }
+const initialValues = { description: "", strength: "", reason_to_choose: "" }
 const UniqueSelling = () => {
+  const [values, setValues] = useState()
+  const [error, setError] = useState(null)
+  const [id, setId] = useState()
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const { data } = await formGetData(
+          "/business/usp",
+          localStorage.getItem("token")
+        )
+        console.log(data)
+        if (data.usp) {
+          setId(data.usp["_id"])
+          initialValues.description = data.usp.description
+          initialValues.strength = data.usp.strength
+          initialValues.reason_to_choose = data.usp.reason_to_choose
+          setValues(initialValues)
+        }
+        setError(null)
+      } catch (err) {
+        console.log(err)
+        setError(err.response)
+      }
+    }
+    fetchData()
+  }, [])
   function validate(values) {
     const errors = {}
-    if (!values.sellingPoint) {
-      errors.sellingPoint = "Required"
+    if (!values.description) {
+      errors.description = "Required"
     }
     if (!values.strength) {
       errors.strength = "Required"
     }
-    if (!values.whyYou) {
-      errors.whyYou = "Required"
+    if (!values.reason_to_choose) {
+      errors.reason_to_choose = "Required"
     }
     return errors
   }
-  function handleSubmit(data) {
-    console.log(data)
+  async function handleSubmit(data) {
+    let resData
+    console.log(values)
+    try {
+      if (values) {
+        resData = await patchData(
+          "/business/usp",
+          id,
+          data,
+          localStorage.getItem("token")
+        )
+      } else {
+        resData = await formPostData(
+          "/business/usp",
+          data,
+          localStorage.getItem("token")
+        )
+      }
+      setError(null)
+      console.log(resData)
+    } catch (err) {
+      setError(err.response)
+      console.log(err.response)
+    }
   }
   return (
     <div className="page-content">
@@ -32,15 +82,15 @@ const UniqueSelling = () => {
               validate={validate}
               handleSubmit={handleSubmit}
             >
-              <label htmlFor="sellingPoint">Your unique selling point: </label>
+              <label htmlFor="description">Your unique selling point: </label>
               <Field
                 type="text"
-                id="sellingPoint"
-                name="sellingPoint"
+                id="description"
+                name="description"
                 className="form-control"
               />
               <ErrorMessage
-                name="sellingPoint"
+                name="description"
                 component="div"
                 style={{ color: "red" }}
               />
@@ -61,15 +111,17 @@ const UniqueSelling = () => {
 
               <br />
 
-              <label htmlFor="whyYou">Why should customer choose you? </label>
+              <label htmlFor="reason_to_choose">
+                Why should customer choose you?{" "}
+              </label>
               <Field
                 type="text"
-                id="whyYou"
-                name="whyYou"
+                id="reason_to_choose"
+                name="reason_to_choose"
                 className="form-control"
               />
               <ErrorMessage
-                name="whyYou"
+                name="reason_to_choose"
                 component="div"
                 style={{ color: "red" }}
               />
