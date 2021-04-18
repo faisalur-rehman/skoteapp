ca7d4b55ca85b11eae9959c466eb03e6b503d7fa
faisalur-rehman <66237466+faisalur-rehman@users.noreply.github.com>
2021-04-18 20:34:07 +0500

    Did get post and patch request for AccessAccount component of social media section

--- src/pages/app/AccessAccount.js
+++ src/pages/app/AccessAccount.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Field, ErrorMessage } from "formik"
 import FormikComponent from "./Formik"
+import { formPostData, formGetData, patchData } from "./ApiRequest"
+
 import { Row, Col, Button } from "reactstrap"
 
 const initialValues = {
-  diffAccount: "",
+  has_difference_access: "",
+  info: "",
 }
 
 const AccessAccount = () => {
@@ -14,10 +17,39 @@ const AccessAccount = () => {
   const [clicked, setClicked] = useState(false)
   const [submitted, setSubmitted] = useState(false)
 
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const { data } = await formGetData(
+          "/services/social-media/account-info",
+          localStorage.getItem("token")
+        )
+        console.log(data.payload)
+        if (data.payload) {
+          setId(data.payload["_id"])
+          if (data.payload.has_difference_access) {
+            initialValues.has_difference_access = "true"
+          } else {
+            initialValues.has_difference_access = "false"
+          }
+          initialValues.info = data.payload.info
+          setValues(initialValues)
+        }
+        setError(null)
+      } catch (error) {
+        setError(error.response)
+      }
+    }
+    fetchData()
+  }, [])
+
   const validate = values => {
     const errors = {}
-    if (!values.diffAccount) {
-      errors.diffAccount = "Required"
+    if (!values.has_difference_access) {
+      errors.has_difference_access = "Required"
+    }
+    if (!values.info) {
+      errors.info = "Required"
     }
     return errors
   }
@@ -25,29 +57,39 @@ const AccessAccount = () => {
   async function handleSubmit(data) {
     let resData
     console.log(data)
-    // try {
-    //   if (value) {
-    //     resData = await patchData(
-    //       "/business/content",
-    //       id,
-    //       data,
-    //       localStorage.getItem("token")
-    //     )
-    //   } else {
-    //     resData = await formPostData(
-    //       "/business/content",
-    //       data,
-    //       localStorage.getItem("token")
-    //     )
-    //   }
-    //   setError(null)
-    //   setSubmitted(true)
-    //   console.log(resData)
-    // } catch (err) {
-    //   setError(err.response.data.errors)
-    //   console.log(err.response)
-    //   setSubmitted(false)
-    // }
+    if (data.has_difference_access === "true") {
+      data.has_difference_access = true
+    } else {
+      data.has_difference_access = false
+    }
+    try {
+      if (value) {
+        resData = await patchData(
+          "/services/social-media/account-info",
+          id,
+          data,
+          localStorage.getItem("token")
+        )
+      } else {
+        resData = await formPostData(
+          "/services/social-media/account-info",
+          data,
+          localStorage.getItem("token")
+        )
+      }
+      setError(null)
+      setSubmitted(true)
+      if (data.has_difference_access) {
+        data.has_difference_access = "true"
+      } else {
+        data.has_difference_access = "false"
+      }
+      console.log(resData)
+    } catch (err) {
+      setError(err.response.data.errors)
+      console.log(err.response)
+      setSubmitted(false)
+    }
   }
 
   return (
@@ -68,21 +110,54 @@ const AccessAccount = () => {
               </p>
 
               <label>
-                <Field type="radio" name="diffAccount" value="Yes" />
+                <Field type="radio" name="has_difference_access" value="true" />
                 Yes
               </label>
               <br />
               <label>
-                <Field type="radio" name="diffAccount" value="No" />
+                <Field
+                  type="radio"
+                  name="has_difference_access"
+                  value="false"
+                />
                 No
               </label>
               <br />
               <ErrorMessage
-                name="diffAccount"
+                name="has_difference_access"
                 component="div"
                 style={{ color: "red" }}
               />
+              <p>Do you have an Ad account ID?</p>
+              <label>
+                <Field
+                  type="radio"
+                  name="info"
+                  value="Yes I have an ad account number"
+                />
+                Yes I have an ad account number
+              </label>
+              <br />
+              <label>
+                <Field type="radio" name="info" value="I am not sure" />
+                I’m not sure
+              </label>
+              <br />
 
+              <label>
+                <Field
+                  type="radio"
+                  name="info"
+                  value="I don't have an ad account number"
+                />
+                No
+              </label>
+              <br />
+              <ErrorMessage
+                name="info"
+                component="div"
+                style={{ color: "red" }}
+              />
               <div>
                 <Button type="submit" color="primary" className="w-md mt-3">
                   Submit
