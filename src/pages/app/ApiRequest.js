1af3fc1d3ef20c554b3b0fa55c586e5a59de8cba
faisalur-rehman <66237466+faisalur-rehman@users.noreply.github.com>
2021-05-22 11:14:50 +0500

    Changed baseUrl

--- src/pages/app/ApiRequest.js
+++ src/pages/app/ApiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: "https://securo-group-portal-app.herokuapp.com/",
 })
 
 export async function postData(endpoint, data) {
